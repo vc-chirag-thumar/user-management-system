fdc44f71c416e73696800c8653c87e08e2b72098
vc-chirag-thumar <chirag.thumar@viitor.cloud>
2023-06-23 19:14:43 +0530

    aaa

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,42 +1,51 @@
 import { Component, OnInit } from '@angular/core';
 
+type User = Array<{ fullname: string; email: string; password: string }>;
+
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.scss']
 })
-export class SignupComponent implements OnInit {
-  user: User = {
+export class SignupComponent {
+  user: User = [
+    {
     fullname: '',
-    email: '',
+    email:  '',
     password: ''
-  };
-  submit() {
-    console.log(this.user);
   }
+]
+
+  
+
+
+  
+
+  submit() {
+    
+  }
+
   constructor() {
     localStorage.setItem('myLSkey', JSON.stringify(this.user));
     console.log(localStorage.getItem('myLSkey'));
 
     
   }
+
   hide = true;
   
-  ngOnInit(){ 
-    let user = localStorage.getItem('user');
-  }
-
-  
+  ngOnInit() {}
 
 }
 
-export interface User {
+
+export interface Users {
   fullname: string;
   email: string;
   password:string;
-
-  
 }
+
+
 
 
 
