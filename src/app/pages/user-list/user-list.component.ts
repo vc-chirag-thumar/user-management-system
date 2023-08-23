292f147846cdef60e86c8ede7ce99420ab312d0f
vc-chirag-thumar <chirag.thumar@viitor.cloud>
2023-08-23 19:19:31 +0530

    jjjdsj

--- src/app/pages/user-list/user-list.component.ts
+++ src/app/pages/user-list/user-list.component.ts
@@ -3,9 +3,8 @@ import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { User } from 'src/app/core/models/user.model';
 import { UserListService } from 'src/app/services/user-list.service';
-import {MatDialog} from '@angular/material/dialog';
+import { MatDialog } from '@angular/material/dialog';
 import { DeleteDialogComponent } from 'src/app/dialogs/delete-dialog/delete-dialog.component';
-
 
 const COLUMNS_SCHEMA = [
   {
@@ -13,35 +12,33 @@ const COLUMNS_SCHEMA = [
     type: 'number',
     label: 'Id',
     disabled: true,
-    isAdd:false
+    isAdd: false,
   },
   {
     key: 'fullname',
     type: 'text',
     label: 'Full Name',
-    isAdd:false
+    isAdd: false,
   },
   {
     key: 'email',
     type: 'text',
     label: 'Email',
     disabled: true,
-    isAdd:false
+    isAdd: false,
   },
   {
     key: 'password',
     type: 'text',
     label: 'Password',
-    isAdd:false
+    isAdd: false,
   },
   {
     key: 'isEdit',
     type: 'isEdit',
-    label: 'Action'
+    label: 'Action',
   },
 ];
-
-
 
 @Component({
   selector: 'app-user-list',
@@ -55,16 +52,15 @@ export class UserListComponent implements OnInit, AfterViewInit {
       fullname: '',
       email: '',
       password: '',
-      isEdit:false,
-      isAdd:false
+      isEdit: false,
+      isAdd: false,
     },
   ];
   itemCount = 0;
   length = 0;
-  // records: any[] = [];
   userCopy: User | null = null;
   nextId = 1;
-  newRow:any
+  newRow: any;
 
   displayedColumns: string[] = COLUMNS_SCHEMA.map((col) => col.key);
   columnsSchema: any = COLUMNS_SCHEMA;
@@ -72,22 +68,14 @@ export class UserListComponent implements OnInit, AfterViewInit {
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
-  constructor(private _userService: UserListService, public dialog: MatDialog) {}
+  constructor(
+    private _userService: UserListService,
+    public dialog: MatDialog
+  ) {}
 
   ngOnInit() {
     this.getUserList();
   }
-
-  // getUserList():void {
-  //   this._userService.getUserData().subscribe((res) => {
-  //     this.userList = res;
-  //     this.userList = this.userList.map((user: User) => {
-  //       return { ...user, isEdit: false };
-  //     });
-  //     this.dataSource.data = this.userList;
-  //     this.itemCount = res.length;
-  //   });
-  // }
 
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
@@ -96,50 +84,43 @@ export class UserListComponent implements OnInit, AfterViewInit {
   deleteData(id: number) {
     const dialogRef = this.dialog.open(DeleteDialogComponent, {
       width: '450px', // Set the width of the dialog
-      data: { /* You can pass data to the dialog component if needed */ }
+      data: {
+        /* You can pass data to the dialog component if needed */
+      },
     });
 
-    dialogRef.afterClosed().subscribe(result => {
+    dialogRef.afterClosed().subscribe((result) => {
       if (result === true) {
         this._userService.deleteData(id).subscribe((record) => {
           this.getUserList();
         });
       }
     });
-    
   }
 
   editRow(row: User) {
     this.userCopy = { ...row };
-    this._userService.updateUser(row).subscribe(()=>{
+    this._userService.updateUser(row).subscribe(() => {
       row.isEdit = !row.isEdit;
+      // row.isAdd = !row.isAdd;
     });
   }
+
   saveData(row: User) {
-    this._userService.updateUser(row).subscribe((updatedUser) =>{
+    this._userService.updateUser(row).subscribe((updatedUser) => {
       row.isEdit = false;
-      localStorage.setItem('updatedUser', JSON.stringify(updatedUser));
-      // console.log(localStorage);
-    },
-    (error) => {
-      console.error('An error occurred:', error);
-    }
-    );
+      // localStorage.setItem('updatedUser', JSON.stringify(updatedUser));
+    });
   }
-  cancelData(row:User){
+
+  cancelData(row: User) {
     if (this.userCopy) {
       Object.assign(row, this.userCopy);
-      row.isEdit = false
+      row.isEdit = false;
       this.userCopy = null;
     }
   }
-  
-  addRow() {
-    
-    const newRow = this.prepareNewRow();
-    this.dataSource.data.push(newRow);
-    this.dataSource.data = [...this.dataSource.data]; // Trigger data source update
-  }
+
   prepareNewRow(): User {
     const newRow: User = {
       id: this.nextId,
@@ -147,29 +128,42 @@ export class UserListComponent implements OnInit, AfterViewInit {
       email: '',
       password: '',
       isEdit: true,
-      isAdd:true
+      isAdd: false,
     };
     this.nextId++; // Increment the next available id for the next row
+    console.log(newRow);
     return newRow;
   }
+  addRow() {
+    const newRow = this.prepareNewRow();
+    this.dataSource.data.push(newRow);
+    this.dataSource.data = [...this.dataSource.data]; // Trigger data source update
+    newRow.isAdd = !newRow.isAdd;
+
+  }
+
+  saveNewRow(row: User) {
+    const saveNewRow = this.dataSource.data[this.dataSource.data.length - 1];
+    saveNewRow.isEdit = false;
+    // newRow.isAdd = !newRow.isAdd;
+    // console.log(saveNewRow);
+    this._userService.postUserData(saveNewRow).subscribe();
+  }
+
   getMaxExistingId() {
     const maxId = this.userList.reduce((max, user) => Math.max(max, user.id), 0);
     this.nextId = maxId + 1;
   }
+
   getUserList(): void {
     this._userService.getUserData().subscribe((res) => {
       this.userList = res;
       this.userList = this.userList.map((user: User) => {
-        return { ...user, isEdit: false };
+        return { ...user, isEdit: false, isAdd: false };
       });
       this.dataSource.data = this.userList;
       this.itemCount = res.length;
       this.getMaxExistingId(); // Call it here to get the maximum existing id
     });
   }
-  saveNewRow(newRow:User){
-    newRow.isAdd = !newRow.isAdd;
-    console.log(newRow);
-  }
-  
 }
